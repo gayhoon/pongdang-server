dd2340098696af40fdcf4b31814b2b5e77b56b10
gayhoon <30451447+gayhoon@users.noreply.github.com>
2025-03-21 18:18:08 +0900

    EC2 healthcheck 추가

---
+++ src/main/java/com/example/pongdang/ec2/HealthCheckController.java
@@ -0,0 +1,16 @@
+package com.example.pongdang.ec2;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("pong");
+    }
+}
