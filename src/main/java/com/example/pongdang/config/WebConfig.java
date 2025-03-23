35d2b49286f0362ef4afea17f44c774566a6aad6
gayhoon <gayhoon@naver.com>
2025-03-23 22:11:24 +0900

    파일업로드 경로 지정

--- src/main/java/com/example/pongdang/config/WebConfig.java
+++ src/main/java/com/example/pongdang/config/WebConfig.java
@@ -8,35 +8,30 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-                        .allowedOriginPatterns("*") // 프론트엔드 URL 허용 로컬일때 [체인지]
-                        .allowedOrigins("https://www.pongdangserver.shop") // 운영일때  [체인지]
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH") // 허용할 HTTP 메서드
-                        .allowCredentials(true) // 쿠키 전송 허용
-                        .allowedHeaders("*")
-                        .exposedHeaders("Set-Cookie"); // JWT 헤더 노출
-            }
+    @Value("${file.upload-dir}")
+    private String uploadDir;
 
-            @Value("${file.upload-dir}")
-            private String uploadDir;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedOrigins("https://www.pongdangserver.shop")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .exposedHeaders("Set-Cookie");
+    }
 
-            @Override
-            public void addResourceHandlers(ResourceHandlerRegistry registry){
-                registry.addResourceHandler("/uploads/**")
-                        .addResourceLocations("file:" + uploadDir + "/") // yml에서 동적으로 불러오기
-                        .setCachePeriod(3600); // 캐시 유지 시간 (1시간)
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:" + uploadDir + "/")
+                .setCachePeriod(3600);
 
-                registry.addResourceHandler("/uploads/profile/**")
-                        .addResourceLocations("file:" + uploadDir + "/profile/") // yml에서 동적으로 불러오기
-                        .setCachePeriod(3600); // 캐시 유지 시간 (1시간)
-            }
-        };
+        registry.addResourceHandler("/uploads/profile/**")
+                .addResourceLocations("file:" + uploadDir + "/profile/")
+                .setCachePeriod(3600);
     }
 }
